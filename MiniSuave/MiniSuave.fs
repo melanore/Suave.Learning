1690c95f42cda223daeb45cbe4c21ee7c546e6ac
melanore <max@socialcee.com>
2018-03-05 18:51:05 +0100

    choose

--- MiniSuave/MiniSuave.fs
+++ MiniSuave/MiniSuave.fs
@@ -3,12 +3,19 @@ module MiniSuave
 open Suave.Http
 open Suave.Console
 open Suave.Successful
+open Suave.Filters
+open Suave.Combinators
 open System
 
 [<EntryPoint>]
 let main argv =
-    let request = { Route = ""; Type = GET }
+    let request = { Route = ""; Type = Suave.Http.GET }
     let response = { Content = String.Empty; StatusCode = 200 }
     let context = { Request = request; Response = response }
-    OK "Hello Suave!" |> executeInLoop context
+    
+    let app = Choose [
+                GET >=> Path "/hello" >=> OK "Hello GET"
+    ]
+
+    executeInLoop context app
     0
--- MiniSuave/Suave.fs
+++ MiniSuave/Suave.fs
@@ -8,6 +8,40 @@ module Successful =
         |> Some
         |> async.Return
 
+module Combinators =
+
+    let (>=>) first second context = async {
+        let! firstContext = first context
+        match firstContext with
+        | None          -> return None
+        | Some context  ->
+        return! second context
+    }
+
+module Filters =
+    open Http
+
+    let iff condition context =
+        if condition context 
+        then context |> Some |> async.Return
+        else None |> async.Return
+
+    let GET = iff (fun context -> context.Request.Type = GET)
+
+    let POST = iff (fun context -> context.Request.Type = POST)
+
+    let Path path = iff (fun context -> context.Request.Route = path)
+
+    let rec Choose webparts context = async {
+        match webparts with
+        | []        -> return None
+        | x :: xs   ->
+        let! result = x context
+        match result with
+        | Some x    -> return Some x
+        | None      -> return! Choose xs context
+    }
+
 module Console = 
     open Http
     open System
