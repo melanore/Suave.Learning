6fb891fe4bb0a594e8400e14bd932c81fd34fe4a
melanore <max@socialcee.com>
2018-03-05 23:22:18 +0100

    mockApi

--- GitHubMockApiServer/GitHubMockApiServer/GitHubMockApiServer.fs
+++ GitHubMockApiServer/GitHubMockApiServer/GitHubMockApiServer.fs
@@ -1,6 +1,21 @@
 module GitHubMockApiServer
 
+open Suave
+open Suave.Operators
+open Suave.Successful
+open Suave.Filters
+open System.IO
+
 [<EntryPoint>]
-let main argv =
-    printfn "%A" argv
-    0 // return an integer exit code
+let main _ =
+    let json fileName = 
+        let content = File.ReadAllText fileName
+        content.Replace("\r", "").Replace("\n", "")
+        |> OK >=> Writers.setMimeType "application/json"
+
+    let user  = pathScan "/users/%s"        (fun _ -> "User.json"  |> json)
+    let repos = pathScan "/users/%s/repos"  (fun _ -> "Repos.json" |> json)
+    let mockApi = choose [repos;user]
+    
+    startWebServer defaultConfig mockApi
+    0
