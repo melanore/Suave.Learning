eb586c96362a17cfe2e557eb81839c18238f17d0
max <max@socialcee.com>
2018-03-06 17:04:49 +0100

    generic db layer with mutable memoizing

--- SuaveRestApi/SuaveRestApi/App.fs
+++ SuaveRestApi/SuaveRestApi/App.fs
@@ -1,17 +1,45 @@
 module SuaveRestApi.App
 open SuaveRestApi.Rest.RestFul
 open Suave.Web
+open Suave
+
+type Person = {
+    Id      : int
+    Name    : string
+    Age     : int
+    Email   : string
+}
+
+type Book = {
+    Id          : int
+    Title       : string
+    Description : string option
+    Author      : Person
+}
 
 [<EntryPoint>]
 let main _ =
-    let personWebPart = rest "people" {
-        GetAll      = PersonDb.getPeople
-        Create      = PersonDb.createPerson
-        Update      = PersonDb.updatePerson
-        Delete      = PersonDb.deletePerson
-        GetById     = PersonDb.getPersonById
-        UpdateById  = PersonDb.updatePersonById
-        Exists      = PersonDb.personExists
-    }
-    startWebServer defaultConfig personWebPart
+    let personDb = Db.init<Person>()
+    let bookDb = Db.init<Book>()
+
+    choose [
+        rest "people" {
+            GetAll      = personDb.GetAll
+            Create      = personDb.Create
+            Update      = fun record -> personDb.Update record.Id record
+            Delete      = personDb.Delete
+            GetById     = personDb.GetById
+            UpdateById  = personDb.Update
+            Exists      = personDb.Exists
+        }
+        rest "books" {
+            GetAll      = bookDb.GetAll
+            Create      = bookDb.Create
+            Update      = fun record -> bookDb.Update record.Id record
+            Delete      = bookDb.Delete
+            GetById     = bookDb.GetById
+            UpdateById  = bookDb.Update
+            Exists      = bookDb.Exists
+        }
+    ] |> startWebServer defaultConfig
     0
